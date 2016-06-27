7ccd166e741ab39e82f455e23f2fe78097312dbb
Mraklen <mraklen@mail.ru>
2016-06-27 22:39:27 +0300

    Начало разработки

---
+++ app/src/main/java/com/example/gwent/GameActivity.java
@@ -0,0 +1,24 @@
+package com.example.gwent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Александр on 27.06.2016.
+ */
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_game);
+    }
+
+}
