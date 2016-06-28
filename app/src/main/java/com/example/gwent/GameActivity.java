9755f56ed4cc9894f5746f965ed10e915dfea5c4
Mraklen <mraklen@mail.ru>
2016-06-28 16:55:41 +0300

    Первые наработки

--- app/src/main/java/com/example/gwent/GameActivity.java
+++ app/src/main/java/com/example/gwent/GameActivity.java
@@ -30,7 +30,6 @@ public class GameActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v){
 
-
         ImageView img = new ImageView(this);
         img.setBackgroundResource(R.drawable.card_test_v1);
 
